05d335c84c703327be3dc9f900bcb47b4e954e72
Bowie <bowie@thinkful.com>
2020-02-13 16:59:56 -0800

    init commit

---
+++ seeds/seed.folders.sql
@@ -0,0 +1,6 @@
+INSERT INTO folders (folder_name)
+VALUES
+    ('TODO'),
+    ('Shopping List'),
+    ('Goals'),
+    ('Homework');
---
+++ seeds/seed.notes.sql
@@ -0,0 +1,9 @@
+INSERT INTO notes (title, content)
+VALUES
+    ('02/13/20', 'Shower, Shave, Brush teeth, Eat, Check Email, Do Homework'),
+    ('02/13/20', 'Rice, Garlic, Onions, Jalepenos, Chicken'),
+    ('Goals for February', 'Complete full-stack Capstone project, get in contact with Pivotal Labs, attend 2 meet ups.'),
+    ('Noteful-API', '1. Write Note and Folder service objects for the new tables
+    2. Write routers to perform CRUD operations
+    3. Write an Express server for the Noteful API with the endpoints /notes and /folders
+    4. Refactor the client application to use this API')
